1c050d1030fda4c3a2a75bee70a33e7ec0146149
IsmaelBena <Ismael.Benadjal@gmail.com>
2021-11-10 14:35:44 +0000

    gitignore

--- webscraper.py
+++ webscraper.py
@@ -29,48 +29,90 @@ def createItemSoup(itemUrl):
 
     return itemPageSoup;
 
+def removeNL(arr):
+    arr.remove('\n')
+    for val in arr:
+        if val == '\n':
+            arr.remove('\n')
+    return arr
 
 def extractItemInfo(soup):
     #print(soup)
 
-    #if rarity = equiptment, don't include
     itemInfo = {'name': '',
-                'imageURL': '',
+                'imageUrl': '',
                 'effect': '',
                 'rarity': '',
                 'category': [],
-                'id': 0,
-                'unlock': ''
+                'id': '',
+                'unlock': '',
+                'stats': [],
+                'cooldown': '',
                 }
+    # to get stats, return all tr in the infobox, and get text after the Stats [add], add them to the statsdict by index, then when index reaches 3, make a new statsdict
 
     for result in soup.findAll("table", class_="infoboxtable"):
         #print(result)
-        print(''.join(result.find("th", class_="infoboxname").findAll(text=True)))
+        #print(''.join(result.find("th", class_="infoboxname").findAll(text=True)))
         itemInfo['name'] = ''.join(result.find("th", class_="infoboxname").findAll(text=True))
-        print(result.find("img").get('src'))
+        if result.find("td", class_="infoboxdesc") is None:
+            break
+        else:
+            itemInfo['effect'] = ''.join(result.find("td", class_="infoboxdesc").findAll(text=True)).rstrip()
+        #print(result.find("img").get('src'))
         itemInfo['imageUrl'] = result.find("img").get('src')
         tempVal = ''
         for td in result.findAll("td"):
             if td.has_attr("class") == False:
-                print(td.findAll(text=True)[0])
+                #print(td.findAll(text=True)[0])
                 if (td.findAll(text=True)[0].lower() in itemInfo.keys()):
                     #print(td.findAll(text=True)[0])
                     tempVal = td.findAll(text=True)[0].lower()
+                    #print(tempVal)
                 elif (tempVal != ''):
-                    if tempVal == id:
+                    if (td.findAll(text=True)[0] == '\n'):
+                        break
+                    elif tempVal == id:
                         itemInfo[tempVal] = td.findAll(text=True)[0][:-2]
+                        tempVal = ''
+                    elif tempVal == 'category':
+                        #print(td.findAll(text=True))
+                        itemInfo[tempVal] = td.findAll(text=True)[:-1]
+                        tempVal = ''
+                    elif tempVal == 'cooldown':
+                        itemInfo[tempVal] = td.findAll(text=True)[0].strip()
                         tempVal = ''
                     else:
                         itemInfo[tempVal] = td.findAll(text=True)[0]
                         tempVal = ''
-        
-                
+                    #print(td.findAll(text=True)[0])
                 #if (len(td.findAll(text=True)) == 2) or (td.findAll(text=True)):
                 #    print(td.findAll(text=True))
-        print(itemInfo)
+
+        statsInfoBool = False
+        for tr in result.findAll("tr"):
+            if (tr.findAll(text=True)[-1] == 'Add\n'):
+                statsInfoBool = True
+            elif statsInfoBool:
+                #print(tr.findAll(text=True)[1:])
+                tempArr = removeNL(tr.findAll(text=True)[1:])
+                #print(tempArr)
+                itemInfo['stats'].append({
+                    'stat': tempArr[0][:-1],
+                    'value': tempArr[1][:-1],
+                    'stack': tempArr[2],
+                    'add': tempArr[-1][:-1],
+                })
+            
+        itemInfo['name'] = itemInfo['name'][:-1]
+        itemInfo['id'] = itemInfo['id'][:-1]
+        for key in itemInfo.keys():
+            print(key, ": ", itemInfo[key])
+            #print(itemInfo)
 
 itemLinks = getItemLinks()
 #print(len(itemLinks))
 
-print(itemLinks[0])
-extractItemInfo(createItemSoup(itemLinks[0]))
+#print(itemLinks[0])
+for item in itemLinks:
+    extractItemInfo(createItemSoup(item))
