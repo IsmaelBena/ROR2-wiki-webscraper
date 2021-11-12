75ebe25da2935f0d166291670d76b8a85cf64a1a
IsmaelBena <32837795+IsmaelBena@users.noreply.github.com>
2021-11-12 13:16:08 +0000

    Update updateDB.py

--- updateDB.py
+++ updateDB.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://Desktop-IB:<ECb2PseFIlifY3jQ>@ror2-cluster.viquk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+client = MongoClient('')
 db = client.admin
 serverStatusResult=db.command("serverStatus")
 print(serverStatusResult)
